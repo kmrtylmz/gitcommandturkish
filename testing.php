023fd32fd13bee02e77cac94d8c5759fed9790a8
Mert Yılmaz <k.mrtylmz2@gmail.com>
2019-11-14 13:27:24 +0300

    Three Commit diff branch and merge request master branch

--- testing.php
+++ testing.php
@@ -1,8 +1,32 @@
 <?php 
 
+include "vendor/autoload.php";
 
-echo "Hello world" . PHP_EOL;
+use Nahid\JsonQ\Jsonq;
 
-echo "Add Commit";
+$server_var = json_encode($_SERVER ,JSON_FORCE_OBJECT); 
 
-echo json_encode(print_r($_SERVER), JSON_FORCE_OBJECT);
+$b = @fopen('server.json' , 'w+');
+
+$path = stream_resolve_include_path('server.json');
+
+fwrite($b, $server_var);
+
+fclose($b);
+
+
+$jsonq = new Jsonq($path);
+
+$json_clone = $jsonq->copy();  # Object okudugu zaman direk olarak Garbage Collector oluyor. O yüzden CLone edildi.
+
+echo "REQUEST_METHOD :" . nl2br($jsonq->from('REQUEST_METHOD')->get() . "\n" . "SERVER_PROTOCOL :" . $json_clone->reset()->from('SERVER_PROTOCOL')->get() . "\n");
+
+
+
+$file = $_SERVER['DOCUMENT_ROOT']. $_SERVER['REQUEST_URI'] . 'images' . '/' . 'trees.png';
+
+echo file_exists($file) ?  "Dosya Bulundu" :  "Gitignore engelledi gitmedi."; 
+
+printf("<br /> %d.ncu Commitide atalım" ,  3 );
+
+clearstatcache();
